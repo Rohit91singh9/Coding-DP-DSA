12e19b86fa345b6ed97d7fccbcf034a964c2c722
ROHIT SINGH <31449969+Rohit91singh9@users.noreply.github.com>
2022-07-28 23:05:08 +0530

    Update findEarliestmonth.py

--- findEarliestmonth.py
+++ findEarliestmonth.py
@@ -1,3 +1,4 @@
+# 1st Approach
 def findEarliestmonth(stockPrice): 
     #initialize month variable with 0 
     month=0 
@@ -10,15 +11,35 @@ def findEarliestmonth(stockPrice):
         if(abs(avg1-avg2)<change): 
             change=abs(avg1-avg2) 
             month=len(l) 
-            return month 
-        stockPrice[1,3,2,3] 
-        print("Minimum changes in the month :",findEarliestmonth(stockPrice)) 
+        return month 
+    stockPrice[1,3,2,3] 
+    print("Minimum changes in the month :",findEarliestmonth(stockPrice)) 
+
+
+# 2nd Approach
+def findEarliestMonth(stockPrice):          
+    sum1= 0     
+    sum2= sum(stockPrice)     
+    n= len(stockPrice) 
+
+    mini= 1000000     
+    month= 0  
+
+    for i in range(0, n-1):         
+        sum1 += stockPrice[i]         
+        sum2 -= stockPrice[i]         
+        avg1= sum1 // (i + 1)         
+        avg2= sum2 // (n - i - 1)         
+        if abs(avg2-avg1)< mini:             
+            mini= abs(avg2-avg1)             
+            month= i+1          
+    return month
 
 
 # This is complete logic of a Program
 # Remaining codes are in-built
 
-""""
+"""
 NOTE:
 
     I completed this Assessment during Amazon Hackerrank 1st Round 
