7436846679986b2b68658c346769000f778f1310
ROHIT SINGH <31449969+Rohit91singh9@users.noreply.github.com>
2022-08-06 23:02:01 +0530

    Update Find_Minimum_Character.java

--- Find_Minimum_Character.java
+++ Find_Minimum_Character.java
@@ -1,3 +1,38 @@
+/* 1st Approach */
+
+import java.util.*;  
+class Result { 	 	
+    public static int findMinimumCharacters(String searchWord, String resultWord) { 
+        // Write your code here
+        		
+        int p = 0; 		
+        int q = 0; 		
+        while(p < searchWord.length()) 		
+        { 			
+            if(searchWord.charAt(p) == resultWord.charAt(q)) 			
+            { 				
+                q += 1; 				
+                if(q == resultWord.length()) 				
+                { 					
+                    return 0; 				
+                } 			
+            } 			p += 1; 		
+        } 		return (resultWord.length() - q); 	
+    }  	
+    public static void main(String[] args) throws Exception 	
+    { 		
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter Search Word"); 		
+            String str1 = sc.nextLine(); 		
+            System.out.println("Enter Result Word"); 		
+            String str2 = sc.nextLine(); 		
+            System.out.println(findMinimumCharacters(str1, str2));
+        	
+    } 
+} 
+
+/* 2nd Approach */
+
 import java.util.*;  
 class find { 	 	
     public static int findMinimumCharacters(String searchWord, String resultWord) 	
