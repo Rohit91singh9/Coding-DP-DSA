79cc698a92af6a7f311dd3034eef5ceb1f49eaa4
ROHIT SINGH <31449969+Rohit91singh9@users.noreply.github.com>
2022-08-15 16:54:58 +0530

    Update countMaxOperations.java

--- countMaxOperations.java
+++ countMaxOperations.java
@@ -1,3 +1,5 @@
+// Approach 1
+
 public static int countMaximumOperations(String s, String t) {
     // Write your code here
     int sLength = s.length();
@@ -32,6 +34,44 @@ public static int countMaximumOperations(String s, String t) {
 
     return result == Integer.MAX_VALUE ? -1 : 0;
 }
+
+
+// Approach 2
+
+/*
+public static int countMaxOperations(String s, String t) {
+        int sLength = s.length();
+        Map<Character, Integer> freqS = new HashMap<>();
+
+        for (int i = 0; i < sLength; i++) {
+            char current = s.charAt(i);
+
+            int count = freqS.getOrDefault(current, 0);
+            freqS.put(current, count + 1);
+        }
+
+        int tLength = t.length();
+        Map<Character, Integer> freqT = new HashMap<>();
+
+        for (int i = 0; i < tLength; i++) {
+            char current = t.charAt(i);
+
+            int count = freqT.getOrDefault(current, 0);
+            freqT.put(current, count + 1);
+        }
+
+        int result = Integer.MAX_VALUE;
+
+        for (char key : freqT.keySet()) {
+            int currentCount = freqS.getOrDefault(key, 0);
+            int requiredCount = freqT.get(key);
+
+            result = Math.min(result, currentCount / requiredCount);
+        }
+
+        return result == Integer.MAX_VALUE ? -1 : 0;
+    }
+ */
 
 
 /* NOTE:
