0dbb04e39b674db2926ea7211431e6c4bdb4dfdf
ROHIT SINGH <31449969+Rohit91singh9@users.noreply.github.com>
2022-08-18 16:05:55 +0530

    Create getShoppingCart1.py

---
+++ getShoppingCart1.py
@@ -0,0 +1,46 @@
+class Node:    # Create Node Class
+    def __init__(self, value=None):
+        self.value = value
+        self.next = None
+        return
+    def isempty(self):
+        return self.value == None
+
+class Link():    # Create Link List
+    def __init__(self):   # Initializing head & Tail
+        self.head = None
+        self.tail = None
+    def isempty(self):   # Cheacking for Emptyness
+        return self.head == None
+    def PUSH_HEAD(self,itemName):    # PUsh_HEAD:- Adding element in head of Link list
+        curr = self.head 
+        self.head = Node(itemName)
+        self.head.next = curr
+    def PUSH_TAIL(self,itemName):   # PUsh_TAIL:- Adding element in END of Link list 
+        curr = self.head
+        while (curr.next != None):
+            curr = curr.next
+        curr.next = Node(itemName)
+    def POP_HEAD(self):   # POP_HEAD:- Remuving element from head of Link list
+        if self.isempty():
+            return 'List is Alredy Empty'
+        else:
+            curr = self.head
+            self.head = self.head.next
+            curr = None
+    def __str__(self):   # Displaying Result(Link List)
+       curr = self.head
+       while(curr.next != None):
+           print(str(curr.value))
+           curr = curr.next
+       return str(curr.value)
+   
+l=Link()
+l.PUSH_HEAD('CUP')
+l.PUSH_HEAD('PEN')
+l.PUSH_TAIL('FAN')
+l.PUSH_HEAD('JAM')
+l.POP_HEAD()
+l.POP_HEAD()
+
+print(l)   #Printing
